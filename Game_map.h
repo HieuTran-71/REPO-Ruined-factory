80783a4865f9f295a85d6b67d6a7573cdc839d0e
Hieu Tran <tranhieu7126@gmail.com>
2025-03-04 13:23:17 +0700

    project

---
+++ Game_map.h
@@ -0,0 +1,35 @@
+#ifndef GAME_MAP_H
+#define GAME_MAP_H
+
+#include "CommonFunc.h"
+#include "BaseObject.h"
+
+#define MAX_TILES 20
+
+class TileMat : public BaseObject
+{
+public:
+    TileMat(){;}
+    ~TileMat() {;}
+}; // hinh anh tinh duoc fill vao map
+
+class GameMap
+{
+public:
+    GameMap(){;}
+    ~GameMap(){;}
+
+    void LoadMap(char* name);
+    void LoadTiles(SDL_Renderer* Screen);
+    void DrawMap(SDL_Renderer* screen);
+    Map getMap() const {return game_map_;}; // lay ra map
+    void SetMap(Map& map_data) {game_map_ = map_data;};
+private:
+    Map game_map_;
+    TileMat tile_mat[MAX_TILES];
+}; // quan ly viec xay dung map
+
+
+
+
+#endif // GAME_MAP_H
